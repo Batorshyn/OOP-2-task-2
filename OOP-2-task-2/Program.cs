7893afd951f669567706619d2503c56721cf937d
Batorshyn <batorshyndanil@gmail.com>
2026-09-11 10:00:30 +0300

    Numbers have been added to the filter.

--- OOP-2-task-2/Program.cs
+++ OOP-2-task-2/Program.cs
@@ -3,7 +3,7 @@ using System.Text.RegularExpressions;
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 Console.InputEncoding = System.Text.Encoding.UTF8;
 
-string pattern = @"\b[пp][^а-яa-zA-Zіієє\s]*[оo][^а-яa-zA-Zіієє\s]*[гg][^а-яa-zA-Zіієє\s]*[аa][^а-яa-zA-Zіієє\s]*[нh][а-яa-zA-Zіієє]*\b";
+string pattern = @"\bп[^а-яa-zA-Zіієє0-9\s]*[оo][^а-яa-zA-Zіієє0-9\s]*[гg][^а-яa-zA-Zіієє0-9\s]*[аa][^а-яa-zA-Zіієє0-9\s]*[нh][а-яa-zA-Zіієє]*\b";
 
 while (true)
 {
